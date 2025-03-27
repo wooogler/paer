8c46bc8fa0c2a44e9924d6f911970df97e6396ed
yzheng94 <61158372+yuhangzheng94@users.noreply.github.com>
2025-03-27 17:38:51 -0400

    Paragraph summary/intent is now auto-updated by sentence change

--- server/src/services/paperService.ts
+++ server/src/services/paperService.ts
@@ -22,7 +22,7 @@ export class PaperService {
   }
 
   async updateSentence(blockId: string, content: string): Promise<void> {
-    const parentId: string = this.paperRepository.findParentBlockByChildId(null, blockId);
+    const parentId: string = this.paperRepository.findParentBlockIdByChildId(null, blockId);
     const contextValue: string = this.paperRepository.getChildrenValues(parentId, "content");
     this.autoUpdateParentBlock(parentId, contextValue);
     return this.paperRepository.updateSentence(blockId, content, await this.summarizeSentence(content), await this.findIntent(content));
